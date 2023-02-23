5bc9ce90539feac3f61d9faa033786ca8c7b0465
renping <renping@dreame.tech>
2023-02-23 17:08:13 +0800

    add plugin.zsh file. add readme.org

---
+++ zsh-aliases-exa.plugin.zsh
@@ -0,0 +1,23 @@
+########################################
+# Project:    zsh-aliases-exa
+# File:       zsh-aliases-exa.plugin.zsh
+# Author:     RP
+# Created:    2023-02-23
+########################################
+
+## general
+# quick view
+alias l='exa --icons --color-scale --sort=type -F'
+# detail view
+alias ls='exa --header --icons --color-scale --sort=type --long -g -F'
+# detail with git view
+alias lsg='exa --header --icons --color-scale --sort=type --long -g -F --git'
+# modified sort view
+alias lsm='exa --header --icons --color-scale --sort=modified --long -g -F'
+
+## other
+# tree view
+alias ltree='exa --tree --level=' # append number of level
+# recurse view
+alias lr='exa --recurse'
+
